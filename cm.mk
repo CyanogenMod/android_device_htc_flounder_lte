44c6ee9ece752a631d306616a5408be77810714e
André Pinela <sheffzor@gmail.com>
2015-10-19 20:16:59 +0100

    Get flounder_lte ready for nightlies
    * Add kernel inline
    * Add updated fingerprint and description
    * Sync with flounder wifi
    
    Change-Id: Ifcad53505829472e4e97f01c77531d6f96a47b34

--- cm.mk
+++ cm.mk
@@ -22,11 +22,39 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit device configuration
 $(call inherit-product, device/htc/flounder/aosp_flounder64.mk)
 
+# Inline kernel building
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.8/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_SOURCE := kernel/htc/flounder
+TARGET_KERNEL_CONFIG := flounder_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
+TARGET_PREBUILT_KERNEL := false
+
+# Extra Packages
+PRODUCT_PACKAGES += \
+	com.android.nfc_extras
+
+# CM Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+	device/htc/flounder/overlay-cm
+
+# Enable USB OTG (CAF commit to Settings)
+ADDITIONAL_BUILD_PROPERTIES += \
+	persist.sys.isUsbOtgEnabled=true
+
 # Inherrit LTE config
 $(call inherit-product, device/htc/flounder/device-lte.mk)
 $(call inherit-product-if-exists, vendor/htc/flounder_lte/device-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/lte_only_overlay
+# LTE Overlays 
+DEVICE_PACKAGE_OVERLAYS += \
+	$(LOCAL_PATH)/lte_only_overlay
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=flounder_lte \
+    BUILD_FINGERPRINT=google/volantisg/flounder_lte:5.1.1/LMY48T/2237560:user/release-keys \
+    PRIVATE_BUILD_DESC="volantisg-user 5.1.1 LMY48T 2237560 release-keys"
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_flounder_lte
