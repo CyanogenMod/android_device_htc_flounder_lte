38bc68b8d38e1ee59eae5662e583c9d5ad3dd914
razorloves <razorloves@gmail.com>
2016-07-07 15:25:43 -0700

    flounder_lte: update fingerprint to MOB30P
    Change-Id: I71b36870821842f7c67270c9bebabbcc1d5843e5

--- cm.mk
+++ cm.mk
@@ -54,9 +54,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flounder_lte \
-    BUILD_FINGERPRINT=google/volantisg/flounder_lte:6.0.1/MMB29R/2482564:user/release-keys \
-    PRIVATE_BUILD_DESC="volantisg-user 6.0.1 MMB29R 2482564 release-keys" \
-    BUILD_ID=MMB29R
+    BUILD_FINGERPRINT=google/volantisg/flounder_lte:6.0.1/MOB30P/2960889:user/release-keys \
+    PRIVATE_BUILD_DESC="volantisg-user 6.0.1 MOB30P 2960889 release-keys" \
+    BUILD_ID=MOB30P
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_flounder_lte
